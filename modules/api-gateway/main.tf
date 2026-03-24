1a5805673fce9f87d3f18deb86540b5593034970
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-03-24 00:46:26 -0300

    fix: specifies integration method

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -68,6 +68,7 @@ resource "aws_apigatewayv2_integration" "backend" {
   api_id           = aws_apigatewayv2_api.this.id
   integration_type = "HTTP_PROXY"
   integration_uri  = var.lb_listener_arn
+  integration_method = "ANY"
   connection_type  = "VPC_LINK"
   connection_id    = aws_apigatewayv2_vpc_link.this.id
 }
